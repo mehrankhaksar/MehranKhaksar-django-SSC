a5992cb26032466366a7f1741841ee24b5478036
Mehran Khaksar <mewhrankhaksar@yahoo.com>
2021-03-04 17:22:42 +0330

    Second Feature

---
+++ users/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .forms import UserRegisterForm
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
